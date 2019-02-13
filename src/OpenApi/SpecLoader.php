03621bd2197a26028591aba20ea12624dd08ec5b
Daniel Duarte <danieldd.ar@gmail.com>
2019-02-13 00:32:29 -0300

    Implemented specification extensions ('^x-' fields).

--- src/OpenApi/SpecLoader.php
+++ src/OpenApi/SpecLoader.php
@@ -7,8 +7,13 @@ use OpenSpec\Spec\OpenSpec;
 use Symfony\Component\Yaml\Yaml;
 
 
+// @todo Update to support OpenApi v3.0.0, v3.0.1 and v3.0.2
+
 class SpecLoader
 {
+    /**
+     * @var OpenSpec
+     */
     protected $_openApiSpec = null;
 
     public function __construct()
@@ -19,17 +24,27 @@ class SpecLoader
     protected function _loadOpenApiSpec()
     {
         // Load OpenApi metadata from Yaml file
-        $openApiSpecData = $this->_loadYamlFile(__DIR__ . '/openapi.spec.yml');
+        $openApiSpecData = $this->_loadYamlFile(__DIR__ . '/openapi_v3-0-0.spec.yml');
 
         // Analyze metadata acording to OpenSpec (create OpenSpec for OpenApi, to analize user API specs)
         $this->_openApiSpec = new OpenSpec($openApiSpecData);
     }
 
-    protected function _loadYamlFile($filepath): array
+    protected function _loadYamlFile(string $filepath): array
     {
         $data = Yaml::parseFile($filepath);
         if ($data === null) {
             throw new \RuntimeException("Not valid Yaml format in file '$filepath'.");
+        }
+
+        return $data;
+    }
+
+    protected function _loadYamlString(string $yaml): array
+    {
+        $data = Yaml::parse($yaml);
+        if ($data === null) {
+            throw new \RuntimeException("Not valid Yaml string format.");
         }
 
         return $data;
@@ -42,14 +57,14 @@ class SpecLoader
 
     public function loadApiSpecFromYamlFile(string $filepath)
     {
-        $apiSpecData = Yaml::parseFile($filepath);
+        $apiSpecData = $this->_loadYamlFile($filepath);
 
         return $this->loadApiSpec($apiSpecData);
     }
 
     public function loadApiSpecFromYamlString(string $yaml)
     {
-        $apiSpecData = Yaml::parse($yaml);
+        $apiSpecData = $this->_loadYamlString($yaml);
 
         return $this->loadApiSpec($apiSpecData);
     }
